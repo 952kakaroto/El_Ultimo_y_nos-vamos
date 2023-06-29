78c80b49a9dded8be845796c2b755554210e5e53
952kakaroto <alanmontalvo.333@gmail.com>
2023-06-29 09:37:20 -0600

    Ya_acabé

--- src/main/java/com/mycompany/ultimoproyecto/model/Cliente.java
+++ src/main/java/com/mycompany/ultimoproyecto/model/Cliente.java
@@ -9,5 +9,63 @@ package com.mycompany.ultimoproyecto.model;
  * @author LENOVO
  */
 public class Cliente {
-    
+    private String ID;
+    private String NOMBRE;
+    private String APELLIDO;
+    private String EMAIL;
+    private String TELEFONO;
+
+    public Cliente(String ID, String NOMBRE, String APELLIDO, String EMAIL, String TELEFONO) {
+        this.ID = ID;
+        this.NOMBRE = NOMBRE;
+        this.APELLIDO = APELLIDO;
+        this.EMAIL = EMAIL;
+        this.TELEFONO = TELEFONO;
+    }
+
+    public String getID() {
+        return ID;
+    }
+
+    public void setID(String ID) {
+        this.ID = ID;
+    }
+
+    public String getNOMBRE() {
+        return NOMBRE;
+    }
+
+    public void setNOMBRE(String NOMBRE) {
+        this.NOMBRE = NOMBRE;
+    }
+
+    public String getAPELLIDO() {
+        return APELLIDO;
+    }
+
+    public void setAPELLIDO(String APELLIDO) {
+        this.APELLIDO = APELLIDO;
+    }
+
+    public String getEMAIL() {
+        return EMAIL;
+    }
+
+    public void setEMAIL(String EMAIL) {
+        this.EMAIL = EMAIL;
+    }
+
+    public String getTELEFONO() {
+        return TELEFONO;
+    }
+
+    public void setTELEFONO(String TELEFONO) {
+        this.TELEFONO = TELEFONO;
+    }
+
+    @Override
+    public String toString() {
+        return "Cliente{" + "ID=" + ID + ", NOMBRE=" + NOMBRE + ", APELLIDO=" + APELLIDO + ", EMAIL=" + EMAIL + ", TELEFONO=" + TELEFONO + '}';
+    }
+   
 }
--- src/main/java/com/mycompany/ultimoproyecto/model/Empleados.java
+++ src/main/java/com/mycompany/ultimoproyecto/model/Empleados.java
@@ -9,5 +9,63 @@ package com.mycompany.ultimoproyecto.model;
  * @author LENOVO
  */
 public class Empleados {
+    private String ID;
+    private String NOMBRE;
+    private String TELEFONO;
+    private String PUESTO;
+    private Integer Salario;
+
+    public Empleados(String ID, String NOMBRE, String TELEFONO, String PUESTO, Integer Salario) {
+        this.ID = ID;
+        this.NOMBRE = NOMBRE;
+        this.TELEFONO = TELEFONO;
+        this.PUESTO = PUESTO;
+        this.Salario = Salario;
+    }
+
+    public String getID() {
+        return ID;
+    }
+
+    public void setID(String ID) {
+        this.ID = ID;
+    }
+
+    public String getNOMBRE() {
+        return NOMBRE;
+    }
+
+    public void setNOMBRE(String NOMBRE) {
+        this.NOMBRE = NOMBRE;
+    }
+
+    public String getTELEFONO() {
+        return TELEFONO;
+    }
+
+    public void setTELEFONO(String TELEFONO) {
+        this.TELEFONO = TELEFONO;
+    }
+
+    public String getPUESTO() {
+        return PUESTO;
+    }
+
+    public void setPUESTO(String PUESTO) {
+        this.PUESTO = PUESTO;
+    }
+
+    public Integer getSalario() {
+        return Salario;
+    }
+
+    public void setSalario(Integer Salario) {
+        this.Salario = Salario;
+    }
+
+    @Override
+    public String toString() {
+        return "Empleados{" + "ID=" + ID + ", NOMBRE=" + NOMBRE + ", TELEFONO=" + TELEFONO + ", PUESTO=" + PUESTO + ", Salario=" + Salario + '}';
+    }
     
 }
